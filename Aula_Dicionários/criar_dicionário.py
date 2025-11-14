a40fb320f66a33cf34d67d62f81c16dc558c9338
nadiaselene <nadiasafranca@gmail.com>
2025-11-14 13:41:39 -0300

    criando um dicionário com informações inputadas pelo usuário

---
+++ Aula_Dicionários/criar_dicionário.py
@@ -0,0 +1,11 @@
+## criando um dicionário com categorias definidas e valores inseridos pelo usuário
+
+dados_nadia = {}
+
+dados_nadia["nome"] = input("Qual seu nome completo? ")
+dados_nadia["idade"] = input("Qual sua idade? ")
+dados_nadia["profissão"] = input("Qual sua profissão atual? ")
+dados_nadia["comida"] = input("Qual sua comida preferida? ")
+dados_nadia["animação de estimação"] = input("Você possui algum animal de estimação? ")
+
+print(dados_nadia)
